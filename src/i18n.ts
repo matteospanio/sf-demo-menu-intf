70e899e809b68f7f088928384b656601f8d6b516
Matteo <spartacus990@gmail.com>
2024-05-20 15:46:21 +0200

    update: translation config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,15 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import HttpApi from 'i18next-http-backend';
 
 i18n.use(LanguageDetector)
-    .use(Backend)
+    .use(HttpApi)
     .use(initReactI18next)
     .init({
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
         debug: true,
         // returnObjects: true,
         lng: 'en',
